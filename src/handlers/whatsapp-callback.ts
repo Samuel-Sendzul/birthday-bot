fe00aec67807bc477003f22c1025fd16e1cc29bf
Samuel Sendzul <samuel.sendzul@gmail.com>
2025-01-04 17:41:27 +0200

    refactor: seperate handling logic

--- src/handlers/whatsapp-callback.ts
+++ src/handlers/whatsapp-callback.ts
@@ -40,7 +40,55 @@ function verifiyWebhookPayload(
   return expectedSignature === receivedSignature;
 }
 
-function handleNewTextMessage() {}
+async function handleNewMessage(change: any): Promise<void> {
+  if (change.field === "messages" && "messages" in change.value) {
+    const messages = change.value.messages;
+    for (const message of messages) {
+      if ("text" in message) {
+        const userState = conversationHandler.getUserState(message.from);
+        if (!userState) {
+          const response =
+            await conversationHandler.whatsapp.sendInteractiveReplyButtonsMessage(
+              "27826229622",
+              "Birthday Bot helps you never forget to send a message to your loved ones, friends, or colleagues on their birthday!\n\n" +
+                "Set up a new birthday reminder to receive a notification on their special day, complete with a contact button for quick and easy messaging!",
+              [{ id: "new-birthday", title: "New Birthday" }],
+              "Welcome to Birthday Bot 🥳",
+              "Never forget to send a birthday message again!"
+            );
+          console.debug(`sent interactive message with id '${response}'`);
+          continue;
+        }
+        conversationHandler.handleNewBirthdayConversationMessage(
+          message.from,
+          message.text.body
+        );
+      }
+      if ("interactive" in message) {
+        if (message.interactive.type === "button_reply") {
+          console.debug(
+            `received button reply with id '${message.interactive.button_reply.id}' and title '${message.interactive.button_reply.title}'`
+          );
+          conversationHandler.handleNewBirthdayConversationMessage(
+            message.from,
+            message.interactive.button_reply.title
+          );
+        }
+      }
+    }
+  }
+}
+
+function handleStatusUpdate(change): void {
+  if (change.field === "messages" && "statuses" in change.value) {
+    const statuses = change.value.statuses;
+    for (const status of statuses) {
+      console.debug(
+        `message with id '${status.id}' status updated to '${status.status}' at time ${status.timestamp} - conversation id '${status.conversation.id}' expires '${status.conversation.expiration_timestamp}'`
+      );
+    }
+  }
+}
 
 export async function handleWhatsappCallback(c: Context) {
   const requestBody = await c.req.json();
@@ -57,59 +105,19 @@ export async function handleWhatsappCallback(c: Context) {
   if (entry) {
     const changes = entry[0].changes;
     for (const change of changes) {
+      // Skip changes that aren't related to the configured WhatsApp number
       if (
         change.value.metadata.phone_number_id !==
         process.env.WHATSAPP_BUSINESS_PHONE_NUMBER_ID
       ) {
         continue;
       }
-      // Recieved new message
-      if (change.field === "messages" && "messages" in change.value) {
-        const messages = change.value.messages;
-        for (const message of messages) {
-          if ("text" in message) {
-            const userState = conversationHandler.getUserState(message.from);
-            if (!userState) {
-              const response =
-                await conversationHandler.whatsapp.sendInteractiveReplyButtonsMessage(
-                  "27826229622",
-                  "Birthday Bot helps you never forget to send a message to your loved ones, friends, or colleagues on their birthday!\n\n" +
-                    "Set up a new birthday reminder to receive a notification on their special day, complete with a contact button for quick and easy messaging!",
-                  [{ id: "new-birthday", title: "New Birthday" }],
-                  "Welcome to Birthday Bot 🥳",
-                  "Never forget to send a birthday message again!"
-                );
-              console.debug(`sent interactive message with id '${response}'`);
-              continue;
-            }
-            conversationHandler.handleNewBirthdayConversationMessage(
-              message.from,
-              message.text.body
-            );
-          }
-          if ("interactive" in message) {
-            if (message.interactive.type === "button_reply") {
-              console.debug(
-                `received button reply with id '${message.interactive.button_reply.id}' and title '${message.interactive.button_reply.title}'`
-              );
-              conversationHandler.handleNewBirthdayConversationMessage(
-                message.from,
-                message.interactive.button_reply.title
-              );
-            }
-          }
-        }
-      }
+
+      // Handle new message
+      await handleNewMessage(change);
 
       // Received status updates
-      if (change.field === "messages" && "statuses" in change.value) {
-        const statuses = change.value.statuses;
-        for (const status of statuses) {
-          console.debug(
-            `message with id '${status.id}' status updated to '${status.status}' at time ${status.timestamp} - conversation id '${status.conversation.id}' expires '${status.conversation.expiration_timestamp}'`
-          );
-        }
-      }
+      handleStatusUpdate(change);
     }
   }
 
